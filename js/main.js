eb3a461dc814600dd6977919b3e13f9c384d2772
abyss3142 <39198514+Min3142@users.noreply.github.com>
2018-06-16 16:16:33 +0300

    2U 4U

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 ﻿function page2() {
          window.location.href = 'https://duefuck.github.io/universe/index.html';
 }
+function mute () {
+    document.getElementById('player').muted = !document.getElementById('player').muted;
+}
